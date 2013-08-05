22abbedc817924bef017758e97985bf7ec0324f2
Everaldo Gomes <everaldo.gomes@gmail.com>
2013-08-05 17:29:00 -0300

    initial commit - Rails 4 Project without ActiveRecord and TestUnit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TecnoLeague::Application.config.secret_key_base = 'a3fbc3693dbdc00db61479da91ecd444e41f50a0eed70ee4782605a46fadf5814e8426fd7ea37b16ee0cc769d3323659188d266b611f750c7128b96a546acb6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TecnoLeague::Application.config.session_store :cookie_store, key: '_tecno_league_session'
